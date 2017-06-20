bb35f609ea1179c2b5a71951afcf28a7063d5aa0
Bri Mariscal <BMariscal@users.noreply.github.com>
2017-06-19 17:10:08 -0700

    fixed 0 button bug
    console.logs added for debugging & fixed issues with the 0 button

--- js/index.js
+++ js/index.js
@@ -5,36 +5,38 @@ let current_operation = ""; //values calculated
 
 
 function myFunction(x) {
-  
-  
+
+
   //Calculate and display answer after '=' button is pressed
   function checkVal() {
     console.log(current_operation);
     try{
-      
+
         let answer = eval(current_operation);
     // display.value = answer.toPrecision(4);
         display.value = answer
         if (list.length === 13 && !(isNaN(list[list.length-1]))) {
             list = [];
             list.push(answer);
+            console.log(current_operation)
         }else {
             list = [];
             list.push(answer);
+            console.log(current_operation)
           }
     }catch(e){
          display.value = 'ERROR'
        }
   }
-  
+
     //When AC button's pressed
   function allCancel() {
     current_operation = "";
     display.value = "0";
     list = [];
   }
-  
-  
+
+
   //When CE button's pressed
   function cancelEntry() {
     let number = current_operation;
@@ -57,22 +59,26 @@ function myFunction(x) {
   }
   //builds current_operation/calls checkVal() when '=' is pressed/calls AC and CE functions
   if (x) {
+    console.log(x)
     if (x === "=") {
       checkVal();
-    } 
+    }
     else {
       list.push(x);
       display.value = list.join("");
       if (x === "x") {
         current_operation += "*";
+        console.log(current_operation)
       } else if (x === "÷") {
         current_operation += "/";
+        console.log(current_operation)
       } else if (x === "AC") {
         allCancel();
       } else if (x === "CE") {
         cancelEntry();
       } else {
         current_operation += x;
+        console.log(current_operation)
       }
     }
   }
